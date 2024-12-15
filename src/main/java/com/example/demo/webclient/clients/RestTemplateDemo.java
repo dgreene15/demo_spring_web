7fb8724003907532fe24be992c4f85657ddbfa11
Darren <dgreene15@yahoo.com>
2024-12-15 09:06:56 -0500

    added rest template

---
+++ src/main/java/com/example/demo/webclient/clients/RestTemplateDemo.java
@@ -0,0 +1,32 @@
+package com.example.demo.webclient.clients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * RestTemplate
+ * - replaced in Spring with WebClient
+ * - below code keeps getting exception on IllegalArgumentException
+ *      - did seperate app with same code and it worked, retry this once other dependencies removed
+ */
+@SpringBootApplication
+public class RestTemplateDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestTemplateDemo.class, args);
+
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://jsonplaceholder.typicode.com/users/1   ";
+
+        try {
+            ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+            System.out.println(response.getBody());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
+
+}
