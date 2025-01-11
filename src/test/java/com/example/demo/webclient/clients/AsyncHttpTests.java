fc7e022802566d5cb2c7bbe48400f0c4cbed20e7
Darren <dgreene15@yahoo.com>
2025-01-11 08:10:08 -0500

    added http test

---
+++ src/test/java/com/example/demo/webclient/clients/AsyncHttpTests.java
@@ -0,0 +1,26 @@
+package com.example.demo.webclient.clients;
+
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
+
+public class AsyncHttpTests {
+
+    @Test
+    void testHttpAsync() {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://jsonplaceholder.typicode.com/posts"))
+                .build();
+
+        CompletableFuture<HttpResponse<String>> responseFuture = client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+
+        responseFuture.thenApply(HttpResponse::body)
+                .thenAccept(System.out::println)
+                .join();
+    }
+}
